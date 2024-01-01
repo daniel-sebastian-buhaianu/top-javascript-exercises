4274b9ee1e9b82c0fc5570a832a8adb81917c9b2
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-01-01 07:27:48 +0200

    Solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const arrayOfChars = string.split('');
+  const reversedArrayOfChars = arrayOfChars.reverse();
+  return reversedArrayOfChars.join('');
 };
 
 // Do not edit below this line
