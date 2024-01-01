5570eeac165c07ed56f9a17ad0c074ccd38e2e5e
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-01-01 07:56:31 +0200

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, lastNumber) {
+  if (typeof firstNumber !== "number"
+      || typeof lastNumber !== "number"
+      || firstNumber < 0
+      || lastNumber < 0) return 'ERROR'; 
+  const min = firstNumber < lastNumber ? firstNumber : lastNumber;
+  const max = firstNumber > lastNumber ? firstNumber : lastNumber;
+  let sum = 0;
+  for (let i = min; i <= max; i++) sum += i;
+  return sum;
 };
 
 // Do not edit below this line
