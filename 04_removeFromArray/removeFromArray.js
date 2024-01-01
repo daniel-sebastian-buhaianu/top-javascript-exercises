6f47a1f0156efb132de57c4aae46a3c8cd0dbf28
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-01-01 07:46:10 +0200

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+  elements.forEach(function(element) {
+    let filteredArray = array.filter(function(arrayElement) {
+      return arrayElement !== element;
+    });
+    array = filteredArray;
+  });
+  return array;
 };
 
 // Do not edit below this line
