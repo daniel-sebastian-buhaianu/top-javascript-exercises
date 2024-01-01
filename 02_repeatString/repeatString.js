aca5011d6ce2ae1211df1ce6d348483f430a35f0
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-01-01 06:48:19 +0200

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfTimes) {
+  if (numberOfTimes < 0) return "ERROR";
+  let newString = '';
+  while (numberOfTimes > 0)
+  {
+    newString += string;
+    numberOfTimes--;
+  }
+  return newString;
 };
 
 // Do not edit below this line
