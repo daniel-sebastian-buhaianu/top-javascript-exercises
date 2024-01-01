e2dffea27d86a3fdff8157dfb096c1a813622a85
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-01-01 08:15:41 +0200

    Solve exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFahrenheit) {
+  const degreesCelsius = (degreesFahrenheit-32)*5/9;
+  // return degreesCelsius using one decimal point
+  return Math.round(degreesCelsius*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
+  const degreesFahrenheit = degreesCelsius*(9/5)+32;
+  // return degreesFahrenheit using one decimal point
+  return Math.round(degreesFahrenheit*10)/10;
 };
 
 // Do not edit below this line
