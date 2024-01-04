0932ad36560bd9bd6d6312e814ae6d7937c8c846
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-01-04 05:20:24 +0200

    Rezolva exercitiul 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,18 @@
-const add = function() {
-	
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const sum = array => array.reduce((acc, value) => acc + value, 0);
+const multiply = array => array.reduce((acc, value) => acc * value, 1);
+const power = (number, exponent) => {
+  let p = 1;
+  for (let e = 0; e < exponent; e++)
+    p *= number;
+  return p;
 };
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = a => {
+  let f = 1;
+  for (let i = 2; i <= a; i++)
+    f *= i;
+  return f;	
 };
 
 // Do not edit below this line
