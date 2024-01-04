a20634e7c4cac796e7fb61e4377392b3842d6b2c
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-01-04 06:48:34 +0200

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = people => {
+  const currentYear = (new Date()).getFullYear();
+  let oldest = people[0];
+  let lastYearAlive = oldest.yearOfDeath
+                      ? oldest.yearOfDeath
+                      : currentYear;
+  let maxAge = lastYearAlive - oldest.yearOfBirth;
+  for (let i = 1; i < people.length; i++)
+  {
+    lastYearAlive = people[i].yearOfDeath
+                    ? people[i].yearOfDeath
+                    : currentYear;
+    const age = lastYearAlive - people[i].yearOfBirth; 
+    if (age > maxAge)
+    {
+      maxAge = age;
+      oldest = people[i];
+    }
+  }
+  return oldest;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
