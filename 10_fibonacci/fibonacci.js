01621881c3bd790726debf2428a710b92249a9fe
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-01-04 06:24:50 +0200

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = index => {
+  if (index < 0)
+    return "OOPS";
+  if (index == 0)
+    return 0;
+  if (index == 1)
+    return 1;
+  let x = 0;
+  let y = 1;
+  let z;
+  for (let i = 2; i <= index; i++)
+  {
+    z = x + y;
+    x = y;
+    y = z;
+  }
+  return z;
 };
 
 // Do not edit below this line
